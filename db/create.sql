34ddb35f3d54a31b2d9754bfa58721720e3964d9
DavidBeugelsdijk <112309@emmauscollege.nl>
2021-11-04 17:09:20 +0000

    rating gefixed

--- db/create.sql
+++ db/create.sql
@@ -55,6 +55,7 @@ insert into products (name, description, code, price, rating_id) values ('Squid 
 
 Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 30, "3");
 
+DROP TABLE IF EXISTS ratings;
 CREATE TABLE rating (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   rating TEXT
