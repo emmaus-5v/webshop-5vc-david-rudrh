dd436e56801598899730963c4abe629d353e3ef3
DavidBeugelsdijk <112309@emmauscollege.nl>
2021-11-04 16:53:34 +0000

    rating aangepast

--- db/create.sql
+++ db/create.sql
@@ -9,7 +9,7 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2),
-  rating TEXT
+  rating_id INTEGER
 );
 
 
@@ -42,4 +42,9 @@ insert into products (name, description, code, price, rating) values ('FIFA 23',
 insert into products (name, description, code, price, rating) values ('My Hero Academia RPG', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
 
 Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 30, "9.5/10");
+
+CREATE TABLE rating (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  rating TEXT
+);
 
