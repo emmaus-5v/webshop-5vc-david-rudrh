e65baed47cf71580ef98c96e0a7fef55d82ef625
DavidBeugelsdijk <112309@emmauscollege.nl>
2021-11-04 19:09:17 +0000

    eerste 4 tabellen

--- db/create.sql
+++ db/create.sql
@@ -22,38 +22,41 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, rating_id) values ('GTA VI', 'Follow-up game of GTA V. A game in which you can do whatever you want, primarily being a criminal. Oh, and you can race as well.', '891778674-5', 49.95, "5");
+insert into products (name, description, code, price, rating_id) values ('GTA VI', 'Follow-up game of GTA V. A game in which you can do whatever you want, primarily being a criminal. Oh, and you can race as well.', '891778674-5', 49.95, 5);
 insert into products (name, description, code, price, rating_id) values ('Hide Online 2', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
 
 Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
 
-Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', '077030122-3', 20, "2");
+Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', '077030122-3', 20, 2);
 insert into products (name, description, code, price, rating_id) values ('Pokémon Legends: Arceus', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
 
 Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
 
-Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', '445924201-X', 60, "3");
+Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', '445924201-X', 60, 3);
 insert into products (name, description, code, price, rating_id) values ('ARK 2', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
 
 In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
 
-Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', '693155505-7', 45, "1");
-insert into products (name, description, code, price, rating_id) values ('FIFA 23', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 50, "2");
+Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', '693155505-7', 45, 1);
+insert into products (name, description, code, price, rating_id) values ('FIFA 23', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 50, 2);
 insert into products (name, description, code, price, rating_id) values ('My Hero Academia RPG', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
 
-Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 30, "4");
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 30, 4);
 insert into products (name, description, code, price, rating_id) values ('Call of Duty: Black Ops 6', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
 
-Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 60, "3");
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 60, 3);
 insert into products (name, description, code, price, rating_id) values ('One Piece RPG', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
 
-Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 40, "4");
-insert into products (name, description, code, price, rating_id) values ('Robooy Simulator', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 40, 4);
+insert into products (name, description, code, price, rating_id) values ('Robooy Hunt Quest', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
 
-Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 13, "1");
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 13, 1);
 insert into products (name, description, code, price, rating_id) values ('Squid Game Online', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
 
-Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 30, "3");
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 30, 3);
+insert into products (name, description, code, price, rating_id) values ('Blimbs TD Battles', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 4,95, 3);
 
 DROP TABLE IF EXISTS rating;
 CREATE TABLE rating (
@@ -76,30 +79,46 @@ insert into typegame (typegame) values ("RPG");
 insert into typegame (typegame) values ("MMORPG");
 insert into typegame (typegame) values ("Survival");
 insert into typegame (typegame) values ("Shooter");
-insert into typegame (typegame) values ("RPG");
-insert into typegame (typegame) values ("Plaform");
+insert into typegame (typegame) values ("Platform");
 insert into typegame (typegame) values ("Battle Royale");
 insert into typegame (typegame) values ("Horror");
 insert into typegame (typegame) values ("Sport");
 insert into typegame (typegame) values ("Racing");
 insert into typegame (typegame) values ("Wargame");
 insert into typegame (typegame) values ("Tower Defense");
+insert into typegame (typegame) values ("Open World");
+insert into typegame (typegame) values ("Multiplayer")
 
 DROP TABLE IF EXISTS typegamenaam;
 CREATE TABLE typegamenaam (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   products_id INTEGER,
   typegame_id INTEGER
-
 );
 
 insert into typegamenaam (products_id, typegame_id) values (1, 2);
+insert into typegamenaam (products_id, typegame_id) values (1, 12);
+insert into typegamenaam (products_id, typegame_id) values (1, 13);
 insert into typegamenaam (products_id, typegame_id) values (2, 3);
+insert into typegamenaam (products_id, typegame_id) values (2, 13);
 insert into typegamenaam (products_id, typegame_id) values (3, 1);
+insert into typegamenaam (products_id, typegame_id) values (3, 12);
 insert into typegamenaam (products_id, typegame_id) values (4, 3);
-insert into typegamenaam (products_id, typegame_id) values (5, 9);
+insert into typegamenaam (products_id, typegame_id) values (4, 12);
+insert into typegamenaam (products_id, typegame_id) values (4, 13);
+insert into typegamenaam (products_id, typegame_id) values (5, 8);
+insert into typegamenaam (products_id, typegame_id) values (5, 13);
 insert into typegamenaam (products_id, typegame_id) values (6, 1);
+insert into typegamenaam (products_id, typegame_id) values (6, 12);
 insert into typegamenaam (products_id, typegame_id) values (7, 4);
+insert into typegamenaam (products_id, typegame_id) values (7, 10);
+insert into typegamenaam (products_id, typegame_id) values (7, 13);
 insert into typegamenaam (products_id, typegame_id) values (8, 1);
-insert into typegamenaam (products_id, typegame_id) values (9, 8);
-insert into typegamenaam (products_id, typegame_id) values (10,3);
+insert into typegamenaam (products_id, typegame_id) values (9, 3);
+insert into typegamenaam (products_id, typegame_id) values (9, 5);
+insert into typegamenaam (products_id, typegame_id) values (9, 7);
+insert into typegamenaam (products_id, typegame_id) values (10, 3);
+insert into typegamenaam (products_id, typegame_id) values (10, 6);
+insert into typegamenaam (products_id, typegame_id) values (10, 9);
+insert into typegamenaam (products_id, typegame_id) values (11, 10);
+insert into typegamenaam (products_id, typegame_id) values (11, 13);
